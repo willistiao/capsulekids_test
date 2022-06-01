4cbdeca13a5a93c2bd47f124dd9bacb43d6fd54c
Willis <tiaowillis2@gmail.com>
2022-06-01 13:03:21 -0700

    three.js boilerplate

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import regeneratorRuntime from "regenerator-runtime";
-// import * as THREE from "three";
+import * as THREE from "three";
 // import { InteractionManager } from "three.interactive";
 
 alert('hello');
@@ -58,3 +58,33 @@ alert('hello');
 //   TWEEN.update(time);
 // });
 
+
+
+// init
+
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animation );
+document.body.appendChild( renderer.domElement );
+
+// animation
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
